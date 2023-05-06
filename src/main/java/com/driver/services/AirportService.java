23e0097478b53cac93bf50040aedf2e7d6bb8e6d
jainpranshu21 <pranshujain80@gmail.com>
2023-05-07 00:46:04 +0530

    uploading 6th api

--- src/main/java/com/driver/services/AirportService.java
+++ src/main/java/com/driver/services/AirportService.java
@@ -5,13 +5,11 @@ import com.driver.model.City;
 import com.driver.model.Flight;
 import com.driver.model.Passenger;
 import com.driver.repositories.AirportRepositories;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class AirportService {
 
@@ -61,4 +59,24 @@ public class AirportService {
         return -1;
     }
 
+
+    public int getNumberOfPeopleOn(Date date,String airportName) {
+        int noOfPassengers=0;
+        List<Airport>airports=airportRepositories.getLargestAirportName();
+        City city=City.BANGLORE;
+        for(Airport airport:airports){
+            if(airport.getAirportName().equals(airportName))
+                city=airport.getCity();
+
+        }
+        List<Flight>flights=airportRepositories.getShortestDurationOfPossibleBetweenTwoCities();
+        for(Flight flight:flights){
+            if(flight.getFlightDate()==date){
+                if(flight.getToCity().equals(city) || flight.getFromCity().equals(city)){
+                    noOfPassengers+=flight.getMaxCapacity();
+                }
+            }
+        }
+        return noOfPassengers;
+    }
 }
